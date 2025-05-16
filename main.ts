8adae95bf3fa663eb66e3584529d4b8f95096b49
Arash Azad <broowser@gmail.com>
2025-05-16 09:33:50 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,33 @@
-basic.forever(function () {
-    // Vooruit draaien
-    basic.showString(">")
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(2000)
-    // Stoppen
-    basic.showString("-")
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(1000)
-    // Achteruit draaien
-    basic.showString("<")
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
-    // Stoppen
-    basic.showString("-")
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(1000)
-})
+/**
+ * DRV8833 motorbesturing via P0 en P1
+ */
+//% color="#AA278D" weight=100 icon="\uf085"
+namespace motor {
+
+    /**
+     * Laat de motor vooruit draaien
+     */
+    //% block="Motor vooruit draaien"
+    export function vooruit() {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+
+    /**
+     * Laat de motor achteruit draaien
+     */
+    //% block="Motor achteruit draaien"
+    export function achteruit() {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+
+    /**
+     * Stopt de motor
+     */
+    //% block="Motor stoppen"
+    export function stop() {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+}
